4fa173e5e532e36e65b7502d51e91f4aca1b9571
Jan Müller <jan.mueller@codecentric.de>
2021-05-28 08:29:51 +0200

    DEEP-DIVE-MVC-Index: 07_spring_mvc_deep_dive

---
+++ 07_spring_mvc_deep_dive/src/test/java/inc/monster/controllers/IndexControllerTest.java
@@ -0,0 +1,24 @@
+package inc.monster.controllers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+public class IndexControllerTest {
+
+    private IndexController indexController = new IndexController();
+
+    private MockMvc mockMvc = MockMvcBuilders.standaloneSetup(indexController).build();
+
+    @Test
+    public void testIndex() throws Exception {
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("index"));
+    }
+
+}
