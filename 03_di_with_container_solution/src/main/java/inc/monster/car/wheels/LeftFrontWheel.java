3ed7b588c8ebd80391d6fdcfbc3eb81a5d866965
Jan Müller <jan.mueller@codecentric.de>
2021-05-28 08:29:50 +0200

    SOLUTION-BASE: 03_di_with_container_solution

---
+++ 03_di_with_container_solution/src/main/java/inc/monster/car/wheels/LeftFrontWheel.java
@@ -0,0 +1,14 @@
+package inc.monster.car.wheels;
+
+public class LeftFrontWheel {
+
+    private String wheelName = "left front wheel";
+
+    public void drive() {
+        System.out.println(wheelName + " is spinning");
+    }
+
+    public void stop() {
+        System.out.println(wheelName + " is stopping");
+    }
+}
---
+++ 03_di_with_container_solution/src/main/java/inc/monster/car/wheels/RightRearWheel.java
@@ -0,0 +1,14 @@
+package inc.monster.car.wheels;
+
+public class RightRearWheel {
+
+    private String wheelName = "right rear wheel";
+
+    public void drive() {
+        System.out.println(wheelName + " is spinning");
+    }
+
+    public void stop() {
+        System.out.println(wheelName + " is stopping");
+    }
+}
