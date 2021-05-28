c82dd14cc4e4c3d21bf1579f3f8038595ce05126
Jan Müller <jan.mueller@codecentric.de>
2021-05-28 08:29:51 +0200

    DEEP-DIVE-MVC-DeleteProduct: 07_spring_mvc_deep_dive

--- 07_spring_mvc_deep_dive/src/main/java/inc/monster/controllers/ProductController.java
+++ 07_spring_mvc_deep_dive/src/main/java/inc/monster/controllers/ProductController.java
@@ -5,6 +5,7 @@ import inc.monster.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,10 +13,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 @Controller
 public class ProductController {
 
-    private ProductService productService;
+    private final ProductService productService;
 
     @Autowired
-    public void setProductService(ProductService productService) {
+    public ProductController(ProductService productService) {
         this.productService = productService;
     }
 
@@ -57,6 +58,15 @@ public class ProductController {
         model.addAttribute("product", productService.getProductById(id));
 
         return "productform";
+    }
+
+    // Remember this is not a REST-Api, we are requesting a view from the server
+    // The alternative would be to use a <form> with POST in the view
+    @GetMapping("/product/delete/{id}")
+    public String delete(@PathVariable Integer id) {
+        productService.deleteProduct(id);
+
+        return "redirect:/products";
     }
 
 }
--- 07_spring_mvc_deep_dive/src/test/java/inc/monster/controllers/IndexControllerTest.java
+++ 07_spring_mvc_deep_dive/src/test/java/inc/monster/controllers/IndexControllerTest.java
@@ -5,8 +5,8 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 public class IndexControllerTest {
 
@@ -17,8 +17,8 @@ public class IndexControllerTest {
     @Test
     public void testIndex() throws Exception {
         mockMvc.perform(get("/"))
-                .andExpect(status().isOk())
-                .andExpect(view().name("index"));
+                .andExpect(status().isFound())
+                .andExpect(header().string("Location", "/products"));
     }
 
 }
