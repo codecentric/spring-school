b76bfe5e1c337bfe628c967e019fd2657db7f297
Jan Müller <jan.mueller@codecentric.de>
2021-05-28 08:29:51 +0200

    SOLUTION-COMPLETE: 04_autoconfiguration_solution

--- 04_autoconfiguration_solution/src/main/java/inc/monster/autoconfiguration/AutoconfigurationApplication.java
+++ 04_autoconfiguration_solution/src/main/java/inc/monster/autoconfiguration/AutoconfigurationApplication.java
@@ -2,12 +2,16 @@ package inc.monster.autoconfiguration;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnJava;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnJava.Range;
+import org.springframework.boot.system.JavaVersion;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class AutoconfigurationApplication {
 
 	@Bean
+	@ConditionalOnJava(range = Range.EQUAL_OR_NEWER, value = JavaVersion.NINE)
 	MyComponent loadMyComponent() {
 		return 	new MyComponent();
 	}
