89ac21e0f82cfcbed3ddcb2d5e837a5ec08829c8
Jan Müller <jan.mueller@codecentric.de>
2021-12-09 08:09:21 +0100

    EXERCISE: 08_spring_mvc_crud_customer

---
+++ 08_spring_mvc_crud_customer/src/test/java/inc/monster/controllers/IndexControllerTest.java
@@ -0,0 +1,25 @@
+package inc.monster.controllers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(IndexController.class)
+public class IndexControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void testIndex() throws Exception {
+        mockMvc.perform(get("/"))
+                .andExpect(status().isFound())
+                .andExpect(header().string("Location", "/products"));
+    }
+
+}
