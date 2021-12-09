64a169085bda4f5ad547ed33501b14aafafdde39
Jan Müller <jan.mueller@codecentric.de>
2021-12-09 08:09:21 +0100

    DEEP-DIVE-MVC-ValidationLocalisation: 07_spring_mvc_deep_dive

---
+++ 07_spring_mvc_deep_dive/Readme.md
@@ -0,0 +1,5 @@
+# Spring MVC Deep Dive
+
+1. Probiert es im Browser aus
+2. Schaut euch den Java Code an
+3. Schaut euch die Views an
---
+++ 07_spring_mvc_deep_dive/src/main/java/inc/monster/LocaleConfig.java
@@ -0,0 +1,33 @@
+package inc.monster;
+
+import java.util.Locale;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+@Configuration
+public class LocaleConfig implements WebMvcConfigurer {
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        SessionLocaleResolver slr = new SessionLocaleResolver();
+        slr.setDefaultLocale(Locale.US);
+        return slr;
+    }
+
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
+        lci.setParamName("lang");
+        return lci;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor());
+    }
+}
