ce2f496aa2428be663a466eb33ec713377ebaa8c
Jan Müller <jan.mueller@codecentric.de>
2021-12-09 08:09:21 +0100

    SOLUTION-BASE: 08_spring_mvc_crud_customer_solution

---
+++ 08_spring_mvc_crud_customer_solution/src/main/java/inc/monster/controllers/IndexController.java
@@ -0,0 +1,13 @@
+package inc.monster.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    @GetMapping("/")
+    public String index() {
+        return "redirect:/products";
+    }
+}
