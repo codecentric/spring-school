3f4830ecee22cdafc44e8c9d9682b2337cd1c9a3
Jan Müller <jan.mueller@codecentric.de>
2021-12-09 08:09:19 +0100

    EXERCISE: 03_di_with_container

---
+++ 03_di_with_container/src/main/java/inc/monster/car/wheels/RightFrontWheel.java
@@ -0,0 +1,14 @@
+package inc.monster.car.wheels;
+
+public class RightFrontWheel {
+
+    private String wheelName = "right front wheel";
+
+    public void drive() {
+        System.out.println(wheelName + " is spinning");
+    }
+
+    public void stop() {
+        System.out.println(wheelName + " is stopping");
+    }
+}
