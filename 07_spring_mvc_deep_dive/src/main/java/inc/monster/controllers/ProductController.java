98f926623ac2a1cd465c21ae4b5b25eea3d885ed
Jan Müller <jan.mueller@codecentric.de>
2021-12-09 08:09:20 +0100

    DEEP-DIVE-MVC-DisplayProduct: 07_spring_mvc_deep_dive

--- 07_spring_mvc_deep_dive/src/main/java/inc/monster/controllers/ProductController.java
+++ 07_spring_mvc_deep_dive/src/main/java/inc/monster/controllers/ProductController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class ProductController {
@@ -22,5 +23,13 @@ public class ProductController {
         model.addAttribute("products", productService.listAllProducts());
 
         return "products";
+    }
+
+    @GetMapping("/product/{id}")
+    public String getProduct(@PathVariable Integer id, Model model) {
+
+        model.addAttribute("product", productService.getProductById(id));
+
+        return "product";
     }
 }
--- 07_spring_mvc_deep_dive/src/main/java/inc/monster/services/ProductServiceImpl.java
+++ 07_spring_mvc_deep_dive/src/main/java/inc/monster/services/ProductServiceImpl.java
@@ -22,6 +22,11 @@ public class ProductServiceImpl implements ProductService {
         return new ArrayList<>(products.values());
     }
 
+    @Override
+    public Product getProductById(Integer id) {
+        return products.get(id);
+    }
+
     private void loadProducts() {
         products = new HashMap<>();
 
